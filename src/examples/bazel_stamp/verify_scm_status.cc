950b524e5e9127e19d44ce416625f48812f9d84b
Saumil <saumpsh@gmail.com>
2024-09-27 08:32:22 -0700

    improve stamp example with notes

--- src/examples/bazel_stamp/verify_scm_status.cc
+++ src/examples/bazel_stamp/verify_scm_status.cc
@@ -6,9 +6,9 @@ int BuildTimeStampStatus();
 ::std::string BuildScmRevision();
 
 int main() {
+  ::std::cout << "STABLE_GIT_COMMIT is: " << BuildScmRevision() << std::endl;
   ::std::cout << "BUILD_SCM_STATUS is: " << BuildScmStatus() << std::endl;
   ::std::cout << "BUILD_TIMESTAMP is: " << BuildTimeStampStatus() << std::endl;
-  ::std::cout << "STABLE_GIT_COMMIT is: " << BuildScmRevision() << std::endl;
 
   return 0;
 }
